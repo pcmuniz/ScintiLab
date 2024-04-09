e800db4f356aa47738bfd658abcb884490a32315
Pablo-Notebook <pablopcm04@hotmail.com>
2024-04-09 16:24:32 -0300

    feat: modelo modal do chat

--- proj_scintilab/app_chatbot/views.py
+++ proj_scintilab/app_chatbot/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class PaginaInicialView(View):
+    def get(self, request):
+        return render(request, 'app_chatbot/base.html')
--- proj_scintilab/proj_scintilab/settings.py
+++ proj_scintilab/proj_scintilab/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Recife'
 
 USE_I18N = True
 
@@ -116,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
