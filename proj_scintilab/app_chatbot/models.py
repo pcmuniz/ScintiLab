f571605b9262d4f84eb61030d3f47d646cf24da9
Erico <erico.chen@hotmail.com>
2024-04-24 20:50:59 -0300

    Criei os Models e alterei o Views.py

--- proj_scintilab/app_chatbot/models.py
+++ proj_scintilab/app_chatbot/models.py
@@ -1,5 +1,52 @@
 from django.db import models
 
-class OrdemServico(models.Model):
-    tipo_cliente = [('1', 'Pessoa Física'), ('2', 'Pessoa Jurídica')]
-   
+class DadosCliente(models.Model):
+    nome_cliente = models.CharField(max_length=30, null=False)
+    cpf_cnpj = models.CharField(max_length=20, null=False)
+    rg_ie = models.CharField(max_length=15, null=False)
+    data_nascimento_cliente = models.DateField(null=False)
+    email_cliente = models.CharField(max_length=30, null=False)
+    celular_cliente = models.CharField(max_length=11, null=False)
+    telefone_cliente = models.CharField(max_length=11, null=False)
+    endereco_cliente = models.CharField(max_length=60, null=False)
+    bairro_cliente = models.CharField(max_length=20, null=False)
+    cep_cliente = models.CharField(max_length=9, null=False)
+    cidade_cliente = models.CharField(max_length=20, null=False)
+    uf_cliente = models.CharField(max_length=20, null=False)
+
+
+class DadosComprador(models.Model):
+    nome_comprador = models.CharField(max_length=30, null=False)
+    cpf_cnpj = models.CharField(max_length=20, null=False)
+    rg_ie = models.CharField(max_length=15, null=False)
+    data_nascimento_comprador = models.DateField(null=False)
+    email_comprador = models.CharField(max_length=30, null=False)
+    celular_comprador = models.CharField(max_length=11, null=False)
+    telefone_comprador = models.CharField(max_length=11, null=False)
+    endereco_comprador = models.CharField(max_length=60, null=False)
+    bairro_comprador = models.CharField(max_length=20, null=False)
+    cep_comprador = models.CharField(max_length=9, null=False)
+    cidade_comprador = models.CharField(max_length=20, null=False)
+    uf_comprador = models.CharField(max_length=20, null=False)
+
+
+class DadosCompra(models.Model):
+    nome_loja = models.CharField(max_length=30, null=False)
+    num_nf = models.CharField(max_length=20, null=False)
+    data_compra = models.DateField(null=False)
+    cod_produto = models.CharField(max_length=30, null=False)
+    valor = models.CharField(max_length=11, null=False)
+
+class DadosEquipamento(models.Model):
+    nome_equipamento = models.CharField(max_length=30, null=False)
+    marca = models.CharField(max_length=14, null=False)
+    lacrado = models.CharField(max_length=15, null=False)
+    modelo = models.CharField(max_length=8, null=False)
+    num_serie = models.CharField(max_length=30, null=False)
+    senha_usuario = models.CharField(max_length=11, null=False)
+    defeito = models.CharField(max_length=300, null=False)
+    estado = models.CharField(max_length=300, null=False)
+    acessorios = models.CharField(max_length=300, null=False)
+    observacoes = models.CharField(max_length=300, null=False)
+    arquivos = models.CharField(max_length=300, null=False)
+
--- proj_scintilab/app_chatbot/views.py
+++ proj_scintilab/app_chatbot/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from .forms import OrdemServico
+from .models import DadosCliente, DadosCompra, DadosComprador, DadosEquipamento
 
 class PaginaInicialView(View):
     def get(self, request):
@@ -8,5 +8,29 @@ class PaginaInicialView(View):
     
 class OrdemServicoView(View):
     def get(self, request):
-        form = OrdemServico
-        return render(request, 'app_chatbot/os.html', {'form': OrdemServico})
+        return render(request, 'app_chatbot/os.html')
+    
+    def post(self, request):
+        if request.method == "POST":
+            form = request.POST
+            dados_cliente = DadosCliente(nome_cliente=form["nome_cliente"], cpf_cnpj =form["id_cliente"], rg_ie=form["sub_id_cliente"], data_nascimento_cliente=form["data_nascimento-cliente"],
+                                         email_cliente=form["email_cliente"], celular_cliente=form["celular_cliente"], telefone_cliente=form["telefone_cliente"], endereco_cliente=form["endereco_cliente"],
+                                         bairro_cliente=form["bairro_cliente"], cep_cliente=form["cep_cliente"], cidade_cliente=form["cidade_cliente"], uf_cliente=form["uf_cliente"])
+            dados_cliente.save()
+
+            dados_comprador = DadosComprador(nome_comprador=form["nome_comprador"], cpf_cnpj =form["id_comprador"], rg_ie=form["sub_id_comprador"], data_nascimento_comprador=form["data_nascimento-comprador"],
+                                         email_comprador=form["email_comprador"], celular_comprador=form["celular_comprador"], telefone_comprador=form["telefone_comprador"], endereco_comprador=form["endereco_comprador"],
+                                         bairro_comprador=form["bairro_comprador"], cep_comprador=form["cep_comprador"], cidade_comprador=form["cidade_comprador"], uf_comprador=form["uf_comprador"])
+            dados_comprador.save()
+
+            dados_compra = DadosCompra(nome_loja =form["loja"], num_nf=form["nf"], data_compra=form["data_compra"],
+                                         cod_produto=form["cod_produto"], valor=form["valor"])
+            dados_compra.save()
+
+            dados_equipamento = DadosEquipamento(nome_equipamento=form["equipamento"], marca =form["marca"], lacrado=form["estado_lacre"], modelo=form["modelo"],
+                                         num_serie=form["num_serie"], senha_usuario=form["senha_usuario"], defeito=form["texto_defeito"], estado=form["estado_aparelho"],
+                                         acessorios=form["acessorios"], observacoes=form["observacoes"], arquivos=form["arquivos"])
+            dados_equipamento.save()
+
+
+        return render(request, 'app_chatbot/os.html')
