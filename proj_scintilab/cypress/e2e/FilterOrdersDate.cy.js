b3eb6be18e15d62195460202ce862cf17eecf7f9
Pablo_Fedora <pablopcm04@hotmail.com>
2024-06-12 00:59:49 -0300

    feat: testes

--- proj_scintilab/cypress/e2e/FilterOrdersDate.cy.js
+++ proj_scintilab/cypress/e2e/FilterOrdersDate.cy.js
@@ -1,13 +1,15 @@
-describe('funconário vai filtrar as ordens por data de abertura', () => {
-    it('cenario1', () => {
-        //steps do cenario1
+// FAZER ESSE AQUI!!
+describe('Filtro das ordens de serviço por data', () => {
+    it('Eu, como funcionário, gostaria de filtrar as ordens de serviço por data', () => {
+        cy.visit('http://127.0.0.1:8000/');
+        cy.wait(60);
+        cy.get('.custom-span > a').click();
+        cy.wait(60);
+        cy.get('#id_username').type('projetos2')
+        cy.wait(60);
+        cy.get('#id_password').type('cesar2024')
+        cy.wait(60);
+        cy.get('#customer-register-btn').click()
     })
 
-    it('cenario2', () => {
-        //steps do cenario2
-    })
-
-    it('cenario3', () => {
-        //steps do cenario3
-    })
 })
