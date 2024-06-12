cf63a9b70d326716b919fffaee24bdf60e659e48
Pablo_Fedora <pablopcm04@hotmail.com>
2024-06-12 00:57:03 -0300

    feat: testes FAQ e cancelamento de ordem pelo funcionário

--- proj_scintilab/cypress/e2e/FAQ.cy.js
+++ proj_scintilab/cypress/e2e/FAQ.cy.js
@@ -1,13 +1,11 @@
-describe('cliente visualizar as perguntas frequentes', () => {
-    it('cenario1', () => {
-        //steps do cenario1
+describe('Modal do FAQ', () => {
+    it('Eu, como cliente, gostaria acessar o FAQ', () => {
+        cy.visit('http://127.0.0.1:8000/');
+        cy.wait(120);
+        cy.get('.btn').click();
+        cy.get('.help-icon > img').click();
+        cy.wait(120);
+        cy.get('h2').should('have.text', 'Dúvidas Frequentes');
     })
 
-    it('cenario2', () => {
-        //steps do cenario2
-    })
-
-    it('cenario3', () => {
-        //steps do cenario3
-    })
 })
